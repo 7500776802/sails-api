b5f15729460691044c2426897f8ff1942125c143
7500776802 <37377573+7500776802@users.noreply.github.com>
2019-02-27 13:59:16 +0530

    make it better

---
+++ api/controllers/ClientsController.js
@@ -0,0 +1,13 @@
+/**
+ * ClientsController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+
+
+
+
+};
---
+++ api/models/Clients.js
@@ -0,0 +1,44 @@
+/**
+ * Clients.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+module.exports = {
+  attributes: {
+    id: {
+      type: 'number',
+      autoIncrement: true,
+    },
+
+    name: {
+      type: 'string',
+
+    },
+
+    color: {
+      type: 'string'
+    },
+
+
+
+
+
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+  },
+  datastore: 'mongodb'
+};
